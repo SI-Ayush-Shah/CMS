a7e6f1169b4dde749b191bc2da63fc9455bfa5b0
SI-Ayush-Shah <ayush.shah@sportzinteractive.net>
2025-08-18 15:47:50 +0530

    chore: remove unused 'docs' and 'web' apps, update pnpm-lock.yaml, and configure .npmrc for AWS CodeArtifact

---
+++ apps/backend/index.js
@@ -0,0 +1,22 @@
+// apps/api/index.js
+import Fastify from "fastify";
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async (request, reply) => {
+  return { hello: "world" };
+});
+
+const start = async () => {
+  try {
+    await fastify.listen({ port: 3001, host: "0.0.0.0" });
+    console.log("🚀 Fastify API running at http://localhost:3001");
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
